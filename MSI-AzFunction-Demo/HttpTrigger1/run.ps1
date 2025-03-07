884e184ae8636ef18289ed8adbe4297bee7e8371
Ben Reader <ben.reader@gmail.com>
2025-03-07 14:55:23 +0000

    adding code for new blog post

---
+++ MSI-AzFunction-Demo/HttpTrigger1/run.ps1
@@ -0,0 +1,34 @@
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+#region auth
+if ($env:MSI_SECRET) { $token = (Get-AzAccessToken -resourceUrl "https://graph.microsoft.com/").Token }
+$authHeader = @{ Authorization = "Bearer $token" }
+#endregion
+#region main process
+try {
+    $restParams = @{
+        Method      = 'Get'
+        Uri         = 'https://graph.microsoft.com/beta/devices'
+        Headers     = $authHeader
+        ContentType = 'application/json'
+    }
+    $restCall = Invoke-RestMethod @restParams
+    Write-Output "Devices Found: $($restCall.value.count)"
+    $resp = $restCall.value | ConvertTo-Json -Depth 100
+    $statusCode = [HttpStatusCode]::OK
+    $body = $resp
+}
+catch {
+    Write-Output $_.Exception.Message
+    $statusCode = [HttpStatusCode]::BadRequest
+    $body = $_.Exception.Message
+}
+
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+        StatusCode = $statusCode
+        Body       = $body
+    })
+#endregion
