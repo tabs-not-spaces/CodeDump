e65d47616cd510537a32a6eafaf97ecd85fe3025
Ben Reader <ben.reader@gmail.com>
2025-03-08 13:13:14 +0000

    adding auth block

--- MSI-AzFunction-Demo/HttpTrigger1/run.ps1
+++ MSI-AzFunction-Demo/HttpTrigger1/run.ps1
@@ -5,6 +5,12 @@ param($Request, $TriggerMetadata)
 
 #region auth
 if ($env:MSI_SECRET) { $token = (Get-AzAccessToken -resourceUrl "https://graph.microsoft.com/").Token }
+else {
+    Disable-AzContextAutosave -Scope Process | Out-Null
+    $cred = New-Object System.Management.Automation.PSCredential $env:appId, ($env:secret | ConvertTo-SecureString -AsPlainText -Force)
+    Connect-AzAccount -ServicePrincipal -Credential $cred -Tenant $env:tenantId
+    $token = (Get-AzAccessToken -resourceUrl "https://graph.microsoft.com/").Token
+}
 $authHeader = @{ Authorization = "Bearer $token" }
 #endregion
 #region main process
